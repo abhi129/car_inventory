786f48e69f5fa81c35b987c40dbca51d6fa0eb5d
abhijeet <abhijeet@holidaywagon.com>
2018-11-11 21:21:45 +0530

    dsgdgdg

--- newfile.py
+++ newfile.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
 import pandas as pd
 import datetime
-
-
 import csv
 f = open('/home/tanuj/Downloads/Sorted Connection Histories/ConnectionList(1-50Meters).csv')
 csv_f = csv.reader(f)
@@ -13,8 +15,9 @@ for row in csv_f:
          month = int(row[2][3:5])
          year = int(row[2][6:10])
          a = datetime.datetime(year, month, day)
-         if temp == 1:
-             diff_calendar_days = pd.date_range(a, b)
+        
+         if temp == row[1]:
+             diff_calendar_days = pd.date_range(b, a)
              diff_calendar_days = diff_calendar_days[1:-1]
              if row[1] in set1:
                  temp_list = []
@@ -26,9 +29,16 @@ for row in csv_f:
                 temp=0
              
     if row[3] == "Disconnected":
-         day = int(row[2][0:2])
-         month = int(row[2][3:5])
-         year = int(row[2][6:10])
-         b = datetime.datetime(year, month, day)
-         temp =1 
+        if temp == 0:
+             day = int(row[2][0:2])
+             month = int(row[2][3:5])
+             year = int(row[2][6:10])
+             b = datetime.datetime(year, month, day)
+             temp = row[1]
+                
 print(set1)
+
+         
+     
+
+   
